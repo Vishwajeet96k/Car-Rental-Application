373eac75fc1d171a52e07ef4e65e1b65f4846549
Vishwajeet Kaushalye <vishwajeetkaushalye@gmail.com>
2026-01-18 23:04:48 +0530

    First Commit

---
+++ server/server.js
@@ -0,0 +1,33 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import connectDB from "./configs/db.js";
+import userRouter from "./routes/userRoutes.js";
+import ownerRouter from "./routes/ownerRoutes.js";
+import bookingRouter from "./routes/bookingRoutes.js";
+
+// Initialize Expresss App
+const app = express();
+
+// Connect Database
+await connectDB()
+
+
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => res.send("Server is running"))
+
+app.use('/api/user', userRouter);
+app.use('/api/owner', ownerRouter);
+app.use('/api/bookings', bookingRouter);
+
+const PORT = process.env.PORT || 3000;
+
+// Starting the express server
+
+app.listen(PORT, ()=> console.log(`Server is running on port ${PORT}`)
+
+)
